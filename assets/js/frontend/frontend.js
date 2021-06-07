61da4a35eb440db0e46a576e2e8d9daa8d276748
pritomdip <pritomdip010@gmail.com>
2021-06-07 23:56:22 +0600

    modified frontend js file

--- assets/js/frontend/frontend.js
+++ assets/js/frontend/frontend.js
@@ -13,54 +13,51 @@ window.Project = (function (window, document, $, undefined) {
 	'use strict';
 	var app = {
 		init: function () {
-			$("#tdlw_submit_btn").on("click", function (e) {
-				const todo = $("#tdlw_todo_field").val();
-				const data = {
-					'action': 'tdlw_add_new_todo',
-					todo
-				};
+			$('#tdlw_submit_btn').on('click', app.addTodo);
+			$(document).find("input[id^='singleTodo-']").live('click', app.updateTodo)
+		},
+		addTodo: function () {
+			const todo = $("#tdlw_todo_field").val();
+			const data = {
+				'action': 'tdlw_add_new_todo',
+				todo
+			};
 
-				wp.ajax.send('tdlw_add_new_todo', {
-					data: data,
-					dataType: 'json',
-					success: function (res) {
-						if (res.success) {
-							$("#tdlw_todo_field").val('');
-							$(".todo-wrapper").html('');
-							$(".todo-wrapper").append(res.html);
-						}
-					},
-					error: function (error) {
-						console.log('error', error);
+			wp.ajax.send('tdlw_add_new_todo', {
+				data: data,
+				dataType: 'json',
+				success: function (res) {
+					if (res.success) {
+						$("#tdlw_todo_field").val('');
+						$(".todo-wrapper").html('');
+						$(".todo-wrapper").append(res.html);
 					}
-				});
-			});
-
-
-			$(document).find("input[id^='singleTodo-']").live('click', function () {
-				const updateInput = $(this);
-				console.log(updateInput);
-				const num = this.id.split('-')[1];
-				const data = {
-					'action': 'tdlw_update_todo',
-					'key': num
-				};
-				wp.ajax.send('tdlw_update_todo', {
-					data: data,
-					dataType: 'json',
-					success: function (res) {
-						if (res.success) {
-							updateInput.parent().toggleClass("line");
-						}
-					},
-					error: function (error) {
-						console.log('error', error);
-					}
-				});
+				},
+				error: function (error) {
+					console.log('error', error);
+				}
 			});
 		},
-
-
+		updateTodo: function () {
+			const updateInput = $(this);
+			const num = this.id.split('-')[1];
+			const data = {
+				'action': 'tdlw_update_todo',
+				'key': num
+			};
+			wp.ajax.send('tdlw_update_todo', {
+				data: data,
+				dataType: 'json',
+				success: function (res) {
+					if (res.success) {
+						updateInput.parent().toggleClass("line");
+					}
+				},
+				error: function (error) {
+					console.log('error', error);
+				}
+			});
+		}
 	};
 	$(document).ready(app.init);
 
